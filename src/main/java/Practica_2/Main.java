39d494c21d6ca7b23f96732aeeb10d8a1f0b6775
Fer <Fmeji@hotmail.es>
2023-03-23 20:27:12 +0100

    Commit final

--- src/main/java/Practica_2/Main.java
+++ src/main/java/Practica_2/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            Tabla tabla = new Tabla();
+            tabla.estadoActual();
+            tabla.estadoSiguiente();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
     }
 }
