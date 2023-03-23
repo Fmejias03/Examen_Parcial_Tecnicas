cb8a14aa0e499656233de923700d01d1182f1628
Fer <Fmeji@hotmail.es>
2023-03-23 19:46:44 +0100

    Editado la clase Principal

--- src/main/java/Practica_1/Principal.java
+++ src/main/java/Practica_1/Principal.java
@@ -3,8 +3,6 @@ package Practica_1;
 public class Principal {
 
     public static void main(String[] args) {
-        long puntosTotales = 1000000;
-        double piAproximado = Matematicas.generarNumeroPi(puntosTotales);
-        System.out.println("Aproximación a pi con " + puntosTotales + " puntos: " + piAproximado);
+        System.out.println("El numero pi es: " + Matematicas.generarNumeroPi(1000000) + " (aproximado)");
     }
 }
