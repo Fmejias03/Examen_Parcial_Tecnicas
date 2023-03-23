ebeece69295c7289b14454e1a9909351e44e3d80
Fer <Fmeji@hotmail.es>
2023-03-23 20:09:26 +0100

    Creado arrays

--- src/main/java/Practica_2/Tabla.java
+++ src/main/java/Practica_2/Tabla.java
@@ -1,4 +1,21 @@
 package Practica_2;
 
 public class Tabla {
+    public int[][] tablaActual;
+    public int[][] tablaSiguiente = new int[dimension][dimension];
+    public static int dimension = 30;
+
+    public void estadoActual() {
+        tablaActual = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                System.out.print(tablaActual[i][j] + "");
+            }
+        }
+    }
+
+
+
+
+
 }
